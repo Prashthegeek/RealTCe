a8d9876c8614bc7ac8826ab415bdb49a11fb39be
Prashant Sharma <143080376+Prashthegeek@users.noreply.github.com>
2024-12-13 23:26:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {   //means /api se koi bhi request aaye , then use the backend /server for that.
-//         target: 'http://localhost:5000', // Your backend URL
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),   // Optional: remove /api prefix
-//       },
-//     },
-//   },
-// });
-
 
 // vite.config.js
 import { defineConfig } from 'vite';
